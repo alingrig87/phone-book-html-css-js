0f6c0dfb308f653d3c4fa4ce157e7f323cd110c0
Alin Grigorescu <alin.grig87@gmail.com>
2024-06-20 21:01:02 +0300

    fix: add table header only when table does not contain elements

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const nameInputElement = document.querySelector('#name');
 const phoneInputElement = document.querySelector('#phone');
 const addOrEditContactButton = document.querySelector('.add-contact-btn');
-const table = document.querySelector('#phone-book-table');
+
 const phoneNumberDetails = [];
+
+const table = document.querySelector('#phone-book-table');
+const rows = table.querySelectorAll('tr');
+
+// For loop starts from 1 because the first element with index 0 is th not normal tr
+for (let i = 1; i <= rows.length - 1; i++) {
+	phoneNumberDetails.push({
+		name: rows[i].querySelector('td:nth-child(1)').innerHTML,
+		phoneNumber: rows[i].querySelector('td:nth-child(2)').innerHTML,
+	});
+}
+
 const errorOutputParagraph = document.querySelector('#error-output');
 
 let tableRowToBeEdited = null;
